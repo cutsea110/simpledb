a2023e2124008edc64f37a6f3353dfefd967d9cf
cutsea110 <cutsea110@gmail.com>
2022-01-29 06:51:02 +0900

    trying to implement logrecord

---
+++ src/tx/logrecord.rs
@@ -0,0 +1,53 @@
+use anyhow::Result;
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
+use crate::file::page::Page;
+
+#[derive(FromPrimitive)]
+pub enum TxType {
+    CHECKPOINT = 0,
+    START = 1,
+    COMMIT = 2,
+    ROLLBACK = 3,
+    SETI32 = 4,
+    SETSTRING = 5,
+}
+
+pub enum LogRecord {
+    SetCheckpoint(SetCheckpointRecord),
+    SetStart(SetStartRecord),
+    SetCommit(SetCommitRecord),
+    SetRollback(SetRollbackRecord),
+    SetI32(SetI32Record),
+    SetString(SetStringRecord),
+}
+
+impl LogRecord {
+    pub fn create_log_record(bytes: Vec<u8>) -> Result<Self> {
+        let p = Page::new_from_bytes(bytes);
+        let tx_type: i32 = p.get_i32(0)?;
+
+        match FromPrimitive::from_i32(tx_type) {
+            Some(TxType::CHECKPOINT) => Ok(LogRecord::SetCheckpoint(SetCheckpointRecord {})),
+            Some(TxType::START) => panic!("TODO"),
+            Some(TxType::COMMIT) => panic!("TODO"),
+            Some(TxType::ROLLBACK) => panic!("TODO"),
+            Some(TxType::SETI32) => panic!("TODO"),
+            Some(TxType::SETSTRING) => panic!("TODO"),
+            None => panic!("TODO"),
+        }
+    }
+}
+
+pub struct SetCheckpointRecord {}
+
+pub struct SetStartRecord {}
+
+pub struct SetCommitRecord {}
+
+pub struct SetRollbackRecord {}
+
+pub struct SetI32Record {}
+
+pub struct SetStringRecord {}
