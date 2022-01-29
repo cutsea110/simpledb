9a5f22e46f12245db752e53ed432b4c55a3107ae
cutsea110 <cutsea110@gmail.com>
2022-01-29 10:06:59 +0900

    implement SetStringRecord but around Transaction

--- src/tx/logrecord.rs
+++ src/tx/logrecord.rs
@@ -1,10 +1,16 @@
+use core::fmt;
+use std::{cell::RefCell, mem, sync::Arc};
+
 use anyhow::Result;
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
-use crate::file::page::Page;
+use crate::{
+    file::{block_id::BlockId, page::Page},
+    log::manager::LogMgr,
+};
 
-#[derive(FromPrimitive)]
+#[derive(FromPrimitive, Debug, Eq, PartialEq, Clone, Copy)]
 pub enum TxType {
     CHECKPOINT = 0,
     START = 1,
@@ -14,40 +20,95 @@ pub enum TxType {
     SETSTRING = 5,
 }
 
-pub enum LogRecord {
-    SetCheckpoint(SetCheckpointRecord),
-    SetStart(SetStartRecord),
-    SetCommit(SetCommitRecord),
-    SetRollback(SetRollbackRecord),
-    SetI32(SetI32Record),
-    SetString(SetStringRecord),
+pub trait LogRecord {
+    fn op(&self) -> TxType;
+    fn tx_number(&self) -> i32;
 }
 
-impl LogRecord {
-    pub fn create_log_record(bytes: Vec<u8>) -> Result<Self> {
+impl dyn LogRecord {
+    pub fn create_log_record(bytes: Vec<u8>) -> Result<Box<Self>> {
         let p = Page::new_from_bytes(bytes);
-        let tx_type: i32 = p.get_i32(0)?;
+        let tx_type = FromPrimitive::from_i32(p.get_i32(0)?);
 
-        match FromPrimitive::from_i32(tx_type) {
-            Some(TxType::CHECKPOINT) => Ok(LogRecord::SetCheckpoint(SetCheckpointRecord {})),
-            Some(TxType::START) => Ok(LogRecord::SetStart(SetStartRecord {})),
-            Some(TxType::COMMIT) => Ok(LogRecord::SetCommit(SetCommitRecord {})),
-            Some(TxType::ROLLBACK) => Ok(LogRecord::SetRollback(SetRollbackRecord {})),
-            Some(TxType::SETI32) => Ok(LogRecord::SetI32(SetI32Record {})),
-            Some(TxType::SETSTRING) => Ok(LogRecord::SetString(SetStringRecord {})),
+        match tx_type {
+            Some(TxType::CHECKPOINT) => panic!("TODO"),
+            Some(TxType::START) => panic!("TODO"),
+            Some(TxType::COMMIT) => panic!("TODO"),
+            Some(TxType::ROLLBACK) => panic!("TODO"),
+            Some(TxType::SETI32) => panic!("TODO"),
+            Some(TxType::SETSTRING) => Ok(Box::new(SetStringRecord::new(p)?)),
             None => panic!("TODO"),
         }
     }
 }
 
-pub struct SetCheckpointRecord {}
+pub struct SetStringRecord {
+    txnum: i32,
+    offset: i32,
+    val: String,
+    blk: BlockId,
+}
+impl fmt::Display for SetStringRecord {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "<SETSTRING {} {} {} {}>",
+            self.txnum, self.blk, self.offset, self.val
+        )
+    }
+}
 
-pub struct SetStartRecord {}
+impl LogRecord for SetStringRecord {
+    fn op(&self) -> TxType {
+        TxType::SETSTRING
+    }
+    fn tx_number(&self) -> i32 {
+        self.txnum
+    }
+}
+impl SetStringRecord {
+    pub fn new(p: Page) -> Result<Self> {
+        let tpos = mem::size_of::<i32>();
+        let txnum = p.get_i32(tpos)?;
+        let fpos = tpos + mem::size_of::<i32>();
+        let filename = p.get_string(fpos)?;
+        let bpos = fpos + Page::max_length(filename.len());
+        let blknum = p.get_i32(bpos)?;
+        let blk = BlockId::new(&filename, blknum as u64);
+        let opos = bpos + mem::size_of::<i32>();
+        let offset = p.get_i32(opos)?;
+        let vpos = opos + mem::size_of::<i32>();
+        let val = p.get_string(vpos)?;
 
-pub struct SetCommitRecord {}
+        Ok(Self {
+            txnum,
+            offset,
+            val,
+            blk,
+        })
+    }
+    pub fn write_to_log(
+        lm: Arc<RefCell<LogMgr>>,
+        txnum: i32,
+        blk: BlockId,
+        offset: i32,
+        val: String,
+    ) -> Result<u64> {
+        let tpos = mem::size_of::<i32>();
+        let fpos = tpos + mem::size_of::<i32>();
+        let bpos = fpos + Page::max_length(blk.file_name().len());
+        let opos = bpos + mem::size_of::<i32>();
+        let vpos = opos + mem::size_of::<i32>();
+        let reclen = vpos + Page::max_length(val.len());
 
-pub struct SetRollbackRecord {}
+        let mut p = Page::new_from_size(reclen as usize);
+        p.set_i32(0, TxType::SETSTRING as i32)?;
+        p.set_i32(tpos, txnum)?;
+        p.set_string(fpos, blk.file_name())?;
+        p.set_i32(bpos, blk.number() as i32)?;
+        p.set_i32(opos, offset)?;
+        p.set_string(vpos, val)?;
 
-pub struct SetI32Record {}
-
-pub struct SetStringRecord {}
+        lm.borrow_mut().append(p.contents())
+    }
+}
