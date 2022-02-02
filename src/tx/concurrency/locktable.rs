29ec52431dfe19a925bd66c620b424f9f4882f6e
cutsea110 <cutsea110@gmail.com>
2022-02-03 07:26:34 +0900

    interface for LockTable

--- src/tx/concurrency/locktable.rs
+++ src/tx/concurrency/locktable.rs
@@ -1,1 +1,34 @@
+use anyhow::Result;
+use std::collections::HashMap;
 
+use crate::file::block_id::BlockId;
+
+const MAX_TIME: i64 = 10_000;
+
+pub struct LockTable {
+    locks: HashMap<BlockId, i32>,
+}
+
+impl LockTable {
+    pub fn s_lock(blk: BlockId) -> Result<()> {
+        panic!("TODO")
+    }
+    pub fn x_lock(blk: BlockId) -> Result<()> {
+        panic!("TODO")
+    }
+    pub fn unlock(blk: BlockId) -> Result<()> {
+        panic!("TODO")
+    }
+    fn has_x_lock(blk: BlockId) -> Result<bool> {
+        panic!("TODO")
+    }
+    fn has_other_locks(blk: BlockId) -> Result<bool> {
+        panic!("TODO")
+    }
+    fn waiting_too_long(starttime: i64) -> bool {
+        panic!("TODO")
+    }
+    fn get_lock_val(blk: BlockId) -> Result<i64> {
+        panic!("TODO")
+    }
+}
