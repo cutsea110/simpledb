391d1dc22c98662cadf148e29334179b138ef314
cutsea110 <cutsea110@gmail.com>
2022-02-03 15:52:39 +0900

    for synchronized

--- src/tx/concurrency/locktable.rs
+++ src/tx/concurrency/locktable.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
 use crate::file::block_id::BlockId;
 
@@ -7,28 +10,29 @@ const MAX_TIME: i64 = 10_000;
 
 pub struct LockTable {
     locks: HashMap<BlockId, i32>,
+    l: Arc<Mutex<()>>,
 }
 
 impl LockTable {
-    pub fn s_lock(blk: BlockId) -> Result<()> {
+    pub fn s_lock(&mut self, blk: BlockId) -> Result<()> {
         panic!("TODO")
     }
-    pub fn x_lock(blk: BlockId) -> Result<()> {
+    pub fn x_lock(&mut self, blk: BlockId) -> Result<()> {
         panic!("TODO")
     }
-    pub fn unlock(blk: BlockId) -> Result<()> {
+    pub fn unlock(&mut self, blk: BlockId) -> Result<()> {
         panic!("TODO")
     }
-    fn has_x_lock(blk: BlockId) -> Result<bool> {
+    fn has_x_lock(&self, blk: BlockId) -> Result<bool> {
         panic!("TODO")
     }
-    fn has_other_locks(blk: BlockId) -> Result<bool> {
+    fn has_other_locks(&self, blk: BlockId) -> Result<bool> {
         panic!("TODO")
     }
-    fn waiting_too_long(starttime: i64) -> bool {
+    fn waiting_too_long(&self, starttime: i64) -> bool {
         panic!("TODO")
     }
-    fn get_lock_val(blk: BlockId) -> Result<i64> {
+    fn get_lock_val(&self, blk: BlockId) -> Result<i64> {
         panic!("TODO")
     }
 }
