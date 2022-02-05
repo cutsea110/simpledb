5ce48d1f4f6c0404707ab03bd83f8a6723d2d183
cutsea110 <cutsea110@gmail.com>
2022-02-06 06:40:37 +0900

    implement ConcurrencyMgr

--- src/tx/concurrency/manager.rs
+++ src/tx/concurrency/manager.rs
@@ -1,5 +1,8 @@
+use anyhow::Result;
+
 use std::{
     collections::HashMap,
+    net::ToSocketAddrs,
     sync::{Arc, LockResult, Mutex, Once, ONCE_INIT},
 };
 
@@ -14,6 +17,7 @@ pub struct ConcurrencyMgr {
 }
 
 impl ConcurrencyMgr {
+    // emulate for static member locktbl
     pub fn new() -> Self {
         // make locktbl a static member by singleton pattern
         // ref.) https://stackoverflow.com/questions/27791532/how-do-i-create-a-global-mutable-singleton
@@ -31,5 +35,34 @@ impl ConcurrencyMgr {
                 locks: HashMap::new(),
             }
         }
+    }
+    pub fn s_lock(&mut self, blk: BlockId) -> Result<()> {
+        if self.locks.get(&blk).is_none() {
+            self.locktbl.lock().unwrap().s_lock(blk.clone())?;
+            self.locks.insert(blk, "S".to_string());
+        }
+
+        Ok(())
+    }
+    pub fn x_lock(&mut self, blk: BlockId) -> Result<()> {
+        if !self.has_x_lock(&blk) {
+            self.s_lock(blk.clone())?;
+            self.locktbl.lock().unwrap().x_lock(blk.clone())?;
+            self.locks.insert(blk, "X".to_string());
+        }
+
+        Ok(())
+    }
+    pub fn release(&mut self) -> Result<()> {
+        for blk in self.locks.keys() {
+            self.locktbl.lock().unwrap().unlock(blk.clone())?;
+        }
+        self.locks.clear();
+
+        Ok(())
+    }
+    fn has_x_lock(&self, blk: &BlockId) -> bool {
+        let locktype = self.locks.get(blk);
+        return locktype.is_some() && locktype.unwrap().eq("X");
     }
 }
