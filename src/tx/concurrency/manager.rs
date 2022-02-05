18898a610023677f4fa3d3d61a28db1439b87a6e
cutsea110 <cutsea110@gmail.com>
2022-02-05 13:51:05 +0900

    refactor

--- src/tx/concurrency/manager.rs
+++ src/tx/concurrency/manager.rs
@@ -1,5 +1,7 @@
-use std::collections::HashMap;
-use std::sync::{Arc, LockResult, Mutex, Once, ONCE_INIT};
+use std::{
+    collections::HashMap,
+    sync::{Arc, LockResult, Mutex, Once, ONCE_INIT},
+};
 
 use super::locktable::LockTable;
 use crate::file::block_id::BlockId;
