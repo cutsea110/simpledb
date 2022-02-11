a452e1f2e95bd06586d710afa9e79b173197dfad
cutsea110 <cutsea110@gmail.com>
2022-02-11 11:10:41 +0900

    explicit drop: maybe safely coding

--- src/tx/concurrency/locktable.rs
+++ src/tx/concurrency/locktable.rs
@@ -46,13 +46,12 @@ impl LockTable {
         let timestamp = SystemTime::now();
 
         while !waiting_too_long(timestamp) {
-            {
-                let mut locks = self.locks.lock().unwrap();
-                if !has_x_lock(&locks, blk) {
-                    *locks.entry(blk.clone()).or_insert(0) += 1; // will not be negative
-                    return Ok(());
-                }
+            let mut locks = self.locks.lock().unwrap();
+            if !has_x_lock(&locks, blk) {
+                *locks.entry(blk.clone()).or_insert(0) += 1; // will not be negative
+                return Ok(());
             }
+            drop(locks); // release lock before thread sleep
             thread::sleep(Duration::new(1, 0));
         }
 
@@ -63,14 +62,12 @@ impl LockTable {
         let timestamp = SystemTime::now();
 
         while !waiting_too_long(timestamp) {
-            {
-                let mut locks = self.locks.lock().unwrap();
-                if !has_other_s_locks(&locks, blk) {
-                    *locks.entry(blk.clone()).or_insert(-1) = -1; // means eXclusive lock
-                    return Ok(());
-                }
+            let mut locks = self.locks.lock().unwrap();
+            if !has_other_s_locks(&locks, blk) {
+                *locks.entry(blk.clone()).or_insert(-1) = -1; // means eXclusive lock
+                return Ok(());
             }
-
+            drop(locks); // release lock before thread sleep
             thread::sleep(Duration::new(1, 0));
         }
 
