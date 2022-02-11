baa91b6ce8ef33141353b1be7e21b7a872a6e78f
cutsea110 <cutsea110@gmail.com>
2022-02-11 16:41:20 +0900

    fix

--- src/buffer/manager.rs
+++ src/buffer/manager.rs
@@ -85,6 +85,7 @@ impl BufferMgr {
             if let Ok(buff) = self.try_to_pin(blk) {
                 return Ok(buff);
             }
+            println!("pin: BUSY LOOP");
             thread::sleep(Duration::new(1, 0))
         }
 
@@ -92,43 +93,32 @@ impl BufferMgr {
     }
     // TODO: fix for thread safe
     fn try_to_pin(&mut self, blk: &BlockId) -> Result<Arc<Mutex<Buffer>>> {
-        if let Some(buff) = self.pickup_pinnable_buffer(blk) {
-            // TODO: consider what happen, if an another thread get the same buff and assigned at this point?
-            {
-                let mut b = buff.lock().unwrap();
-
-                b.assign_to_block(blk.clone())?;
-
-                if !b.is_pinned() {
-                    *(self.num_available.lock().unwrap()) -= 1;
-                }
-                b.pin();
+        println!("try_to_pin: CALL find_existing_buffer");
+        let mut buff = self.find_existing_buffer(blk);
+        if buff.is_none() {
+            println!("try_to_pin: NOT found and CALL choose_unpinned_buffer");
+            buff = self.choose_unpinned_buffer();
+            if buff.is_none() {
+                println!("try_to_pin: NOT choose_unpinned_buffer");
+                return Err(From::from(BufferMgrError::BufferAbort));
             }
-
-            return Ok(buff);
+            println!("try_to_pin: choose_unpinned_buffer");
+            let mut b = buff.as_ref().unwrap().lock().unwrap();
+            println!("try_to_pin: assign_to_block");
+            b.assign_to_block(blk.clone())?;
         }
 
-        Err(From::from(BufferMgrError::BufferAbort))
-    }
-    fn pickup_pinnable_buffer(&mut self, blk: &BlockId) -> Option<Arc<Mutex<Buffer>>> {
-        if let Some(buff) = self.find_existing_buffer(blk) {
-            return Some(buff);
+        println!("try_to_pin: is_pinned?");
+        let mut b = buff.as_ref().unwrap().lock().unwrap();
+        if !b.is_pinned() {
+            println!("try_to_pin: NOT is_pinned");
+            *(self.num_available.lock().unwrap()) -= 1;
         }
+        println!("try_to_pin: pin!!");
+        b.pin();
 
-        if let Some(buff) = self.choose_unpinned_buffer() {
-            {
-                let mut b = buff.lock().unwrap();
-
-                if let Err(e) = b.assign_to_block(blk.clone()) {
-                    eprintln!("failed to assign to block: {}", e);
-                    return None;
-                }
-            }
-
-            return Some(buff);
-        }
-
-        None
+        drop(b); // release lock
+        Ok(Arc::clone(&buff.unwrap()))
     }
     fn find_existing_buffer(&self, blk: &BlockId) -> Option<Arc<Mutex<Buffer>>> {
         for i in 0..self.bufferpool.len() {
@@ -192,12 +182,15 @@ mod tests {
         let buff4 = bm.pin(&BlockId::new("testfile", 1));
         println!("Available buffers: {:?}", bm.available());
 
-        println!("Attempting to pin block 3...");
-        let mut buff5 = bm.pin(&BlockId::new("testfile", 3)).unwrap();
-
-        println!("buff5 pinned to block {:?}", buff5.lock().unwrap().block());
+        print!("Attempting to pin block 3...");
+        if let Ok(_) = bm.pin(&BlockId::new("testfile", 3)) {
+            // couldn't come here!
+            println!("Succeed!");
+        } else {
+            println!("Failed!");
+        }
         bm.unpin(Arc::clone(&buff2))?;
-        buff5 = bm.pin(&BlockId::new("testfile", 3)).unwrap(); // now this works
+        let buff5 = bm.pin(&BlockId::new("testfile", 3)).unwrap(); // now this works
 
         println!("Final buffer Allocation:");
         println!(
