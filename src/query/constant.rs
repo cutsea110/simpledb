375d527889a278376254bcf01335b99eb3c335f4
cutsea110 <cutsea110@gmail.com>
2022-02-17 18:18:02 +0900

    implement Constant at chap 8

---
+++ src/query/constant.rs
@@ -0,0 +1,45 @@
+use anyhow::Result;
+use core::fmt;
+
+#[derive(Debug)]
+pub enum ConstantError {
+    TypeError,
+}
+
+impl std::error::Error for ConstantError {}
+impl fmt::Display for ConstantError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::TypeError => {
+                write!(f, "type error")
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum Constant {
+    I32(i32),
+    String(String),
+}
+
+impl Constant {
+    pub fn new_i32(ival: i32) -> Self {
+        Constant::I32(ival)
+    }
+    pub fn new_string(sval: String) -> Self {
+        Constant::String(sval)
+    }
+    pub fn as_i32(&self) -> Result<i32> {
+        match self {
+            Constant::I32(ival) => Ok(*ival),
+            Constant::String(_) => Err(From::from(ConstantError::TypeError)),
+        }
+    }
+    pub fn as_string(&self) -> Result<&str> {
+        match self {
+            Constant::I32(_) => Err(From::from(ConstantError::TypeError)),
+            Constant::String(sval) => Ok(sval),
+        }
+    }
+}
--- src/record/tablescan.rs
+++ src/record/tablescan.rs
@@ -1,1 +1,35 @@
+use anyhow::Result;
+use std::alloc::Layout;
 
+use super::recordpage::RecordPage;
+use crate::tx::transaction::Transaction;
+
+pub struct TableScan {
+    tx: Transaction,
+    layout: Layout,
+    rp: RecordPage,
+    filename: String,
+    currentslot: i32,
+}
+
+impl TableScan {
+    pub fn new(tx: Transaction, tblname: String, layout: Layout) -> Self {
+        panic!("TODO")
+    }
+    // TODO: Methods that implement Scan trait
+    pub fn close(&mut self) -> Result<()> {
+        panic!("TODO")
+    }
+    pub fn before_first(&mut self) -> Result<()> {
+        panic!("TODO")
+    }
+    pub fn next(&self) -> bool {
+        panic!("TODO")
+    }
+    pub fn get_i32(&mut self, fldname: &str) -> Result<i32> {
+        panic!("TODO")
+    }
+    pub fn get_string(&mut self, fldname: &str) -> Result<String> {
+        panic!("TODO")
+    }
+}
