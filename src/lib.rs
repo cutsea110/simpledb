996c87a4a7b9cb4ddd1ebe51d5d72e1a997ce242
cutsea110 <cutsea110@gmail.com>
2022-02-18 00:17:51 +0900

    remove no mean test

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,3 @@ pub mod query;
 pub mod record;
 pub mod server;
 pub mod tx;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
