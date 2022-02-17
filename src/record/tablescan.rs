90646246a91444b4f425ac1979e2348703768083
cutsea110 <cutsea110@gmail.com>
2022-02-17 18:28:25 +0900

    define functions for TableScan

--- src/record/tablescan.rs
+++ src/record/tablescan.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use std::alloc::Layout;
 
-use super::recordpage::RecordPage;
-use crate::tx::transaction::Transaction;
+use super::{recordpage::RecordPage, rid::RID};
+use crate::{query::constant::Constant, tx::transaction::Transaction};
 
 pub struct TableScan {
     tx: Transaction,
@@ -30,6 +30,42 @@ impl TableScan {
         panic!("TODO")
     }
     pub fn get_string(&mut self, fldname: &str) -> Result<String> {
+        panic!("TODO")
+    }
+    pub fn get_val(&self, fldname: &str) -> Constant {
+        panic!("TODO")
+    }
+    pub fn has_field(&self, fldname: &str) -> bool {
+        panic!("TODO")
+    }
+    pub fn set_i32(&mut self, fldname: &str, val: i32) -> Result<()> {
+        panic!("TODO")
+    }
+    pub fn set_string(&mut self, fldname: &str, val: String) -> Result<()> {
+        panic!("TODO")
+    }
+    pub fn set_val(&mut self, fldname: &str, val: Constant) -> Result<()> {
+        panic!("TODO")
+    }
+    pub fn insert(&mut self) -> Result<()> {
+        panic!("TODO")
+    }
+    pub fn delete(&mut self) -> Result<()> {
+        panic!("TODO")
+    }
+    pub fn move_to_ird(&mut self, ird: RID) -> Result<()> {
+        panic!("TODO")
+    }
+    pub fn get_rid(&self) -> RID {
+        panic!("TODO")
+    }
+    fn move_to_block(&mut self, blknum: i32) -> Result<()> {
+        panic!("TODO")
+    }
+    fn move_to_new_block(&mut self) -> Result<()> {
+        panic!("TODO")
+    }
+    fn at_last_block(&self) -> bool {
         panic!("TODO")
     }
 }
