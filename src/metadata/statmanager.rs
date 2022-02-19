f059aabdba77f0227548c914d1ad9327c544a879
cutsea110 <cutsea110@gmail.com>
2022-02-19 16:53:34 +0900

    implement StatMgr

--- src/metadata/statmanager.rs
+++ src/metadata/statmanager.rs
@@ -4,7 +4,10 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use crate::{record::layout::Layout, tx::transaction::Transaction};
+use crate::{
+    record::{layout::Layout, tablescan::TableScan},
+    tx::transaction::Transaction,
+};
 
 use super::tablemanager::TableMgr;
 
@@ -16,21 +19,53 @@ pub struct StatMgr {
 }
 
 impl StatMgr {
-    pub fn new(tbl_mgr: TableMgr, tx: Arc<Mutex<Transaction>>) -> Self {
-        panic!("TODO")
+    pub fn new(tbl_mgr: TableMgr, tx: Arc<Mutex<Transaction>>) -> Result<Self> {
+        let mut mgr = Self {
+            tbl_mgr,
+            tablestats: HashMap::new(), // dummy
+            numcalls: 0,                // dummy
+        };
+
+        mgr.refresh_statistics(tx)?;
+
+        Ok(mgr)
     }
     // synchronized
     pub fn get_stat_info(
-        &self,
+        &mut self,
         tblname: &str,
         layout: Layout,
         tx: Arc<Mutex<Transaction>>,
-    ) -> StatInfo {
-        panic!("TODO")
+    ) -> Result<StatInfo> {
+        self.numcalls += 1;
+
+        if self.numcalls > 100 {
+            self.refresh_statistics(Arc::clone(&tx))?;
+        }
+
+        if let Some(&si) = self.tablestats.get(tblname) {
+            return Ok(si);
+        } else {
+            let si = self.calc_table_stats(tblname, layout, tx)?;
+            self.tablestats.insert(tblname.to_string(), si);
+            return Ok(si);
+        }
     }
     // synchronized
-    pub fn refresh_statistics(&self, tx: Arc<Mutex<Transaction>>) -> Result<()> {
-        panic!("TODO")
+    pub fn refresh_statistics(&mut self, tx: Arc<Mutex<Transaction>>) -> Result<()> {
+        self.tablestats = HashMap::new();
+        self.numcalls += 0;
+        let tcatlayout = self.tbl_mgr.get_layout("tblcat", Arc::clone(&tx))?;
+        let mut tcat = TableScan::new(Arc::clone(&tx), "tblcat", tcatlayout);
+        while tcat.next() {
+            let tblname = tcat.get_string("tblname")?;
+            let layout = self.tbl_mgr.get_layout(&tblname, Arc::clone(&tx))?;
+            let si = self.calc_table_stats(&tblname, layout, Arc::clone(&tx))?;
+            self.tablestats.insert(tblname, si);
+        }
+        tcat.close()?;
+
+        Ok(())
     }
     // synchronized
     pub fn calc_table_stats(
@@ -38,8 +73,17 @@ impl StatMgr {
         tblname: &str,
         layout: Layout,
         tx: Arc<Mutex<Transaction>>,
-    ) -> StatInfo {
-        panic!("TODO")
+    ) -> Result<StatInfo> {
+        let mut num_recs = 0;
+        let mut numblocks = 0;
+        let mut ts = TableScan::new(tx, tblname, layout);
+        while ts.next() {
+            num_recs += 1;
+            numblocks = ts.get_rid().block_number() + 1;
+        }
+        ts.close()?;
+
+        Ok(StatInfo::new(numblocks, num_recs))
     }
 }
 
