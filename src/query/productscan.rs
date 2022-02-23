943873b265e36296857c3e557407b3a14b40d6b8
cutsea110 <cutsea110@gmail.com>
2022-02-24 08:51:03 +0900

    implemented ProductScan

--- src/query/productscan.rs
+++ src/query/productscan.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use core::fmt;
 use std::sync::{Arc, Mutex};
 
-use super::scan::Scan;
+use super::{constant::Constant, scan::Scan};
 
 #[derive(Debug)]
 pub enum ProductScanError {
@@ -30,34 +30,63 @@ pub struct ProductScan {
 }
 
 impl Scan for ProductScan {
-    fn before_first(&mut self) -> anyhow::Result<()> {
-        panic!("TODO")
+    fn before_first(&mut self) -> Result<()> {
+        let mut s1 = self.s1.lock().unwrap();
+        let mut s2 = self.s2.lock().unwrap();
+        s1.before_first()?;
+        s1.next();
+        s2.before_first()
     }
     fn next(&mut self) -> bool {
-        panic!("TODO")
+        let mut s2 = self.s2.lock().unwrap();
+        if s2.next() {
+            true
+        } else {
+            s2.before_first().unwrap();
+            s2.next() && self.s1.lock().unwrap().next()
+        }
     }
-    fn get_i32(&mut self, fldname: &str) -> anyhow::Result<i32> {
-        panic!("TODO")
+    fn get_i32(&mut self, fldname: &str) -> Result<i32> {
+        let mut s1 = self.s1.lock().unwrap();
+        if s1.has_field(fldname) {
+            s1.get_i32(fldname)
+        } else {
+            self.s2.lock().unwrap().get_i32(fldname)
+        }
     }
-    fn get_string(&mut self, fldname: &str) -> anyhow::Result<String> {
-        panic!("TODO")
+    fn get_string(&mut self, fldname: &str) -> Result<String> {
+        let mut s1 = self.s1.lock().unwrap();
+        if s1.has_field(fldname) {
+            s1.get_string(fldname)
+        } else {
+            self.s2.lock().unwrap().get_string(fldname)
+        }
     }
-    fn get_val(&mut self, fldname: &str) -> anyhow::Result<super::constant::Constant> {
-        panic!("TODO")
+    fn get_val(&mut self, fldname: &str) -> Result<Constant> {
+        let mut s1 = self.s1.lock().unwrap();
+        if s1.has_field(fldname) {
+            s1.get_val(fldname)
+        } else {
+            self.s2.lock().unwrap().get_val(fldname)
+        }
     }
     fn has_field(&self, fldname: &str) -> bool {
-        panic!("TODO")
+        self.s1.lock().unwrap().has_field(fldname) || self.s2.lock().unwrap().has_field(fldname)
     }
     fn close(&mut self) -> anyhow::Result<()> {
-        panic!("TODO")
+        let mut s1 = self.s1.lock().unwrap();
+        let mut s2 = self.s2.lock().unwrap();
+        s1.close()?;
+        s2.close()
     }
     fn to_update_scan(&mut self) -> anyhow::Result<&mut dyn super::updatescan::UpdateScan> {
-        panic!("TODO")
+        Err(From::from(ProductScanError::DowncastError))
     }
 }
 
 impl ProductScan {
     pub fn new(s1: Arc<Mutex<dyn Scan>>, s2: Arc<Mutex<dyn Scan>>) -> Self {
+        s1.lock().unwrap().next();
         Self { s1, s2 }
     }
 }
