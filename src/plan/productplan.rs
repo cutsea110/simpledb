2ed3e015a8ede5c7fd121ed6b4b5722c3d345746
cutsea110 <cutsea110@gmail.com>
2022-03-04 12:33:02 +0900

    implement ProductPlan

--- src/plan/productplan.rs
+++ src/plan/productplan.rs
@@ -1,8 +1,11 @@
 use anyhow::Result;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use super::plan::Plan;
-use crate::record::schema::Schema;
+use crate::{
+    query::{productscan::ProductScan, scan::Scan},
+    record::schema::Schema,
+};
 
 pub struct ProductPlan {
     p1: Arc<dyn Plan>,
@@ -11,20 +14,27 @@ pub struct ProductPlan {
 }
 
 impl Plan for ProductPlan {
-    fn open(&self) -> Result<Arc<std::sync::Mutex<dyn crate::query::scan::Scan>>> {
-        panic!("TODO")
+    fn open(&self) -> Result<Arc<Mutex<dyn Scan>>> {
+        let s1 = self.p1.open()?;
+        let s2 = self.p2.open()?;
+
+        Ok(Arc::new(Mutex::new(ProductScan::new(s1, s2))))
     }
     fn blocks_accessed(&self) -> i32 {
-        panic!("TODO")
+        self.p1.blocks_accessed() + (self.p1.records_output() * self.p2.blocks_accessed())
     }
     fn records_output(&self) -> i32 {
-        panic!("TODO")
+        self.p1.records_output() * self.p2.records_output()
     }
     fn distinct_values(&self, fldname: &str) -> i32 {
-        panic!("TODO")
+        if self.p1.schema().has_field(fldname) {
+            return self.p1.distinct_values(fldname);
+        } else {
+            return self.p2.distinct_values(fldname);
+        }
     }
     fn schema(&self) -> Arc<Schema> {
-        panic!("TODO")
+        Arc::clone(&self.schema)
     }
 }
 
