7d9b28e7de96901146a7e0a8ca15ebc60217f75b
cutsea110 <cutsea110@gmail.com>
2022-03-04 17:35:46 +0900

    implement create_query_plan

--- src/plan/planner.rs
+++ src/plan/planner.rs
@@ -1,20 +1,36 @@
 use anyhow::Result;
+use combine::Parser;
 use std::sync::{Arc, Mutex};
 
-use super::{plan::Plan, queryplanner::QueryPlanner, updateplanner::UpdatePlanner};
-use crate::tx::transaction::Transaction;
+use super::{
+    plan::Plan,
+    queryplanner::{self, QueryPlanner},
+    updateplanner::UpdatePlanner,
+};
+use crate::{parser::parser::query, tx::transaction::Transaction};
 
+#[derive(Clone)]
 pub struct Planner {
-    qplanner: Arc<dyn QueryPlanner>,
-    uplanner: Arc<dyn UpdatePlanner>,
+    qplanner: Arc<Mutex<dyn QueryPlanner>>,
+    uplanner: Arc<Mutex<dyn UpdatePlanner>>,
 }
 
 impl Planner {
-    pub fn new(qplanner: Arc<dyn QueryPlanner>, uplanner: Arc<dyn UpdatePlanner>) -> Self {
+    pub fn new(
+        qplanner: Arc<Mutex<dyn QueryPlanner>>,
+        uplanner: Arc<Mutex<dyn UpdatePlanner>>,
+    ) -> Self {
         Self { qplanner, uplanner }
     }
-    pub fn create_query_plan(cmd: String, tx: Arc<Mutex<Transaction>>) -> Arc<dyn Plan> {
-        panic!("TODO")
+    pub fn create_query_plan(
+        &mut self,
+        cmd: &str,
+        tx: Arc<Mutex<Transaction>>,
+    ) -> Result<Arc<dyn Plan>> {
+        let mut parser = query();
+        let (data, _) = parser.parse(cmd)?;
+        // TODO: code to verify the query should be here...
+        self.qplanner.lock().unwrap().create_plan(data, tx)
     }
     pub fn execute_update(cmd: String, tx: Arc<Mutex<Transaction>>) -> Result<i32> {
         panic!("TODO")
