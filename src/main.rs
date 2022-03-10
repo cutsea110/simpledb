07ec540f9fd476a5d9eb7f3e92162cee16f0353a
cutsea110 <cutsea110@gmail.com>
2022-03-10 16:44:27 +0900

    query command tool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,146 @@
+use anyhow::Result;
+use std::{
+    env,
+    io::{stdout, Write},
+    path::Path,
+    process,
+};
+
+use getopts::Options;
+use simpledb::rdbc::{
+    connectionadapter::ConnectionAdapter,
+    driveradapter::DriverAdapter,
+    embedded::{
+        connection::EmbeddedConnection, driver::EmbeddedDriver, resultset::EmbeddedResultSet,
+    },
+    resultsetadapter::ResultSetAdapter,
+    resultsetmetadataadapter::{DataType, ResultSetMetaDataAdapter},
+    statementadapter::StatementAdapter,
+};
+
+#[derive(Debug)]
+struct Args {
+    dbname: String,
+}
+
+fn print_usage(program: &str, opts: &Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+    stdout().flush().unwrap();
+    process::exit(0);
+}
+
+fn parse_args() -> Args {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("d", "dbname", "set database name", "DBNAME");
+    opts.optflag("h", "help", "print this help menu");
+
+    let matches = opts
+        .parse(&args[1..])
+        .unwrap_or_else(|f| panic!("{}", f.to_string()));
+    if matches.opt_present("h") {
+        print_usage(&program, &opts);
+    }
+
+    Args {
+        dbname: matches.opt_str("d").unwrap(),
+    }
+}
+
+fn read_query(conn: &EmbeddedConnection) -> Result<String> {
+    print!("SQL {}> ", conn.get_transaction().lock().unwrap().tx_num());
+    stdout().flush().unwrap();
+
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).ok();
+    Ok(input)
+}
+
+fn print_result_set(results: EmbeddedResultSet) -> Result<()> {
+    // resultset metadata
+    let meta = results.get_meta_data()?;
+    // print header
+    for i in 0..meta.get_column_count() {
+        let name = meta.get_column_name(i).unwrap();
+        let w = meta.get_column_display_size(i).unwrap();
+        print!("{:width$} ", name, width = w);
+    }
+    println!("");
+    // separater
+    for i in 0..meta.get_column_count() {
+        let w = meta.get_column_display_size(i).unwrap();
+        print!("{:-<width$}", "", width = w + 1);
+    }
+    println!("");
+    // scan record
+    let mut c = 0;
+    while results.next() {
+        c += 1;
+        for i in 0..meta.get_column_count() {
+            let fldname = meta.get_column_name(i).unwrap();
+            let w = meta.get_column_display_size(i).unwrap();
+            match meta.get_column_type(i).unwrap() {
+                DataType::Int32 => {
+                    print!("{:width$} ", results.get_i32(fldname)?, width = w);
+                }
+                DataType::Varchar => {
+                    print!("{:width$} ", results.get_string(fldname)?, width = w);
+                }
+            }
+        }
+        println!("");
+    }
+    println!("({} Rows)", c);
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, World!");
+    let args = parse_args();
+    let dbpath = format!("db/{}", args.dbname);
+
+    if !Path::new(&dbpath).exists() {
+        print!("create new '{}'? [Yes/no]> ", args.dbname);
+        stdout().flush().unwrap();
+
+        let mut yes_no = String::new();
+        std::io::stdin().read_line(&mut yes_no).ok();
+        let ans = yes_no.trim().to_ascii_lowercase();
+        if ans != "y" && ans != "yes" {
+            println!("terminates the process.");
+            process::exit(0);
+        }
+    }
+
+    let drvr = EmbeddedDriver::new();
+    if let Ok(mut conn) = drvr.connect(&dbpath) {
+        while let Ok(qry) = read_query(&conn) {
+            if qry.trim().to_ascii_lowercase() == ":q" {
+                conn.close().unwrap();
+                println!("disconnected.");
+                process::exit(0);
+            }
+            let mut stmt = conn.create(&qry).unwrap();
+            let words: Vec<&str> = qry.split_whitespace().collect();
+            if words[0].trim().to_ascii_lowercase() == "select" {
+                if let Ok(result) = stmt.execute_query() {
+                    print_result_set(result).unwrap();
+                } else {
+                    println!("invalid query: {}", qry);
+                }
+            } else {
+                if let Ok(affected) = stmt.execute_update() {
+                    println!("Affected {}", affected);
+                } else {
+                    println!("invalid command: {}", qry);
+                }
+            }
+        }
+    }
+
+    println!("couldn't connect database.");
+    process::exit(1);
 }
