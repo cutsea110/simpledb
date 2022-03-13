f24c398f61688b1db9c5e9e498f8484dba41dcb7
cutsea110 <cutsea110@gmail.com>
2022-03-13 09:39:39 +0900

    refine

--- src/rdbc/embedded/statement.rs
+++ src/rdbc/embedded/statement.rs
@@ -30,27 +30,29 @@ impl<'a> StatementAdapter<'a> for EmbeddedStatement<'a> {
 
     fn execute_query(&'a mut self) -> Result<Self::Set> {
         let tx = self.conn.get_transaction();
-        self.planner
-            .create_query_plan(&self.sql, tx)
-            .and_then(|pln| EmbeddedResultSet::new(pln, &mut self.conn))
-            .or_else(|_| Err(From::from(StatementError::RuntimeError)))
+        match self.planner.create_query_plan(&self.sql, tx) {
+            Ok(pln) => EmbeddedResultSet::new(pln, &mut self.conn),
+            Err(_) => self
+                .conn
+                .rollback()
+                .and(Err(From::from(StatementError::RuntimeError)))
+                .or(Err(From::from(StatementError::RollbackFailed))),
+        }
     }
     fn execute_update(&mut self) -> Result<i32> {
         let tx = self.conn.get_transaction();
-        self.planner
-            .execute_update(&self.sql, tx)
-            .and_then(|affected| {
-                self.conn
-                    .commit()
-                    .and(Ok(affected))
-                    .or(Err(From::from(StatementError::CommitFailed)))
-            })
-            .or({
-                self.conn
-                    .rollback()
-                    .and(Err(From::from(StatementError::RuntimeError)))
-                    .or(Err(From::from(StatementError::RollbackFailed)))
-            })
+        match self.planner.execute_update(&self.sql, tx) {
+            Ok(affected) => self
+                .conn
+                .commit()
+                .and(Ok(affected))
+                .or(Err(From::from(StatementError::CommitFailed))),
+            Err(_) => self
+                .conn
+                .rollback()
+                .and(Err(From::from(StatementError::RuntimeError)))
+                .or(Err(From::from(StatementError::RollbackFailed))),
+        }
     }
     fn close(&mut self) -> Result<()> {
         self.conn
