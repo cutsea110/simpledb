64b5c05181664444b5477f1730287e88ec41eac4
cutsea110 <cutsea110@gmail.com>
2022-03-16 20:27:15 +0900

    implement HashIndex

--- src/index.rs
+++ src/index.rs
@@ -1,13 +1,31 @@
+use core::fmt;
+
 use anyhow::Result;
 
 use crate::{query::constant::Constant, record::rid::RID};
 
 pub mod hash;
 
+#[derive(Debug)]
+pub enum IndexError {
+    NoTableScan,
+}
+
+impl std::error::Error for IndexError {}
+impl fmt::Display for IndexError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            IndexError::NoTableScan => {
+                write!(f, "no table scan")
+            }
+        }
+    }
+}
+
 pub trait Index {
-    fn before_first(&self, searchkey: Constant) -> Result<()>;
-    fn next(&self) -> bool;
-    fn get_data_rid(&self) -> Result<RID>;
+    fn before_first(&mut self, searchkey: Constant) -> Result<()>;
+    fn next(&mut self) -> bool;
+    fn get_data_rid(&mut self) -> Result<RID>;
     fn insert(&mut self, dataval: Constant, datarid: RID) -> Result<()>;
     fn delete(&mut self, dataval: Constant, datarid: RID) -> Result<()>;
     fn close(&mut self) -> Result<()>;
--- src/index/hash.rs
+++ src/index/hash.rs
@@ -1,9 +1,13 @@
 use anyhow::Result;
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+    sync::{Arc, Mutex},
+};
 
-use super::Index;
+use super::{Index, IndexError};
 use crate::{
-    query::constant::Constant,
+    query::{constant::Constant, scan::Scan, updatescan::UpdateScan},
     record::{layout::Layout, rid::RID, tablescan::TableScan},
     tx::transaction::Transaction,
 };
@@ -11,16 +15,22 @@ use crate::{
 pub const NUM_BUCKETS: i32 = 100;
 
 pub struct HashIndex {
-    _tx: Arc<Mutex<Transaction>>,
-    _idxname: String,
-    _layout: Arc<Layout>,
-    _searchkey: Option<Constant>,
-    _ts: Option<TableScan>,
+    tx: Arc<Mutex<Transaction>>,
+    idxname: String,
+    layout: Arc<Layout>,
+    searchkey: Option<Constant>,
+    ts: Option<TableScan>,
 }
 
 impl HashIndex {
-    pub fn new(_tx: Arc<Mutex<Transaction>>, _idxname: String, _layout: Arc<Layout>) -> Self {
-        panic!("TODO")
+    pub fn new(tx: Arc<Mutex<Transaction>>, idxname: String, layout: Arc<Layout>) -> Self {
+        Self {
+            tx,
+            idxname,
+            layout,
+            searchkey: None,
+            ts: None,
+        }
     }
     pub fn search_cost(numblocks: i32, _rpb: i32) -> i32 {
         numblocks / NUM_BUCKETS
@@ -28,22 +38,67 @@ impl HashIndex {
 }
 
 impl Index for HashIndex {
-    fn before_first(&self, _searchkey: Constant) -> Result<()> {
-        panic!("TODO")
+    fn before_first(&mut self, searchkey: Constant) -> Result<()> {
+        self.close()?;
+        self.searchkey = Some(searchkey.clone());
+        let mut hasher = DefaultHasher::new();
+        searchkey.hash(&mut hasher);
+        let bucket = hasher.finish() % NUM_BUCKETS as u64;
+        let tblname = format!("{}{}", self.idxname, bucket);
+        self.ts = TableScan::new(Arc::clone(&self.tx), &tblname, Arc::clone(&self.layout)).ok();
+
+        Ok(())
     }
-    fn next(&self) -> bool {
-        panic!("TODO")
+    fn next(&mut self) -> bool {
+        if let Some(ts) = self.ts.as_mut() {
+            while ts.next() {
+                if ts.get_val("dataval").ok() == self.searchkey {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        false
     }
-    fn get_data_rid(&self) -> Result<RID> {
-        panic!("TODO")
+    fn get_data_rid(&mut self) -> Result<RID> {
+        if let Some(ts) = self.ts.as_mut() {
+            let blknum = ts.get_i32("block")?;
+            let id = ts.get_i32("id")?;
+
+            return Ok(RID::new(blknum, id));
+        }
+
+        Err(From::from(IndexError::NoTableScan))
     }
-    fn insert(&mut self, _dataval: Constant, _datarid: RID) -> Result<()> {
-        panic!("TODO")
+    fn insert(&mut self, val: Constant, rid: RID) -> Result<()> {
+        self.before_first(val.clone())?;
+        if let Some(ts) = self.ts.as_mut() {
+            ts.insert()?;
+            ts.set_i32("block", rid.block_number())?;
+            ts.set_i32("id", rid.slot())?;
+            ts.set_val("dataval", val)?;
+            return Ok(());
+        }
+
+        Err(From::from(IndexError::NoTableScan))
     }
-    fn delete(&mut self, _dataval: Constant, _datarid: RID) -> Result<()> {
-        panic!("TODO")
+    fn delete(&mut self, val: Constant, rid: RID) -> Result<()> {
+        self.before_first(val)?;
+        while self.next() {
+            if self.get_data_rid().unwrap() == rid {
+                self.ts.as_mut().unwrap().delete()?;
+                return Ok(());
+            }
+        }
+
+        Err(From::from(IndexError::NoTableScan))
     }
     fn close(&mut self) -> Result<()> {
-        panic!("TODO")
+        if let Some(ts) = self.ts.as_mut() {
+            return ts.close();
+        }
+
+        Err(From::from(IndexError::NoTableScan))
     }
 }
