93aab68c1a328df9d43236759588e2ef109c6063
cutsea110 <cutsea110@gmail.com>
2022-03-24 11:09:15 +0900

    implement IndexSelectScan

--- src/index/query/indexselectscan.rs
+++ src/index/query/indexselectscan.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
@@ -8,42 +9,72 @@ use crate::{
     record::tablescan::TableScan,
 };
 
+#[derive(Debug)]
+pub enum IndexSelectScanError {
+    DowncastError,
+}
+
+impl std::error::Error for IndexSelectScanError {}
+impl fmt::Display for IndexSelectScanError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            IndexSelectScanError::DowncastError => {
+                write!(f, "downcast error")
+            }
+        }
+    }
+}
+
 pub struct IndexSelectScan {
-    ts: TableScan,
+    ts: Arc<Mutex<TableScan>>,
     idx: Arc<Mutex<dyn Index>>,
     val: Constant,
 }
 
 impl IndexSelectScan {
-    pub fn new(ts: Arc<Mutex<TableScan>>, idx: Arc<Mutex<dyn Index>>, val: Constant) -> Self {
-        panic!("TODO")
+    pub fn new(
+        ts: Arc<Mutex<TableScan>>,
+        idx: Arc<Mutex<dyn Index>>,
+        val: Constant,
+    ) -> Result<Self> {
+        let mut scan = Self { ts, idx, val };
+        scan.before_first()?;
+
+        Ok(scan)
     }
 }
 
 impl Scan for IndexSelectScan {
     fn before_first(&mut self) -> Result<()> {
-        panic!("TODO")
+        self.idx.lock().unwrap().before_first(self.val.clone())
     }
     fn next(&mut self) -> bool {
-        panic!("TODO")
+        let ok = self.idx.lock().unwrap().next();
+        if ok {
+            let rid = self.idx.lock().unwrap().get_data_rid().unwrap();
+            self.ts.lock().unwrap().move_to_rid(rid).unwrap();
+        }
+
+        ok
     }
     fn get_i32(&mut self, fldname: &str) -> Result<i32> {
-        panic!("TODO")
+        self.ts.lock().unwrap().get_i32(fldname)
     }
     fn get_string(&mut self, fldname: &str) -> Result<String> {
-        panic!("TODO")
+        self.ts.lock().unwrap().get_string(fldname)
     }
     fn get_val(&mut self, fldname: &str) -> Result<Constant> {
-        panic!("TODO")
+        self.ts.lock().unwrap().get_val(fldname)
     }
     fn has_field(&self, fldname: &str) -> bool {
-        panic!("TODO")
+        self.ts.lock().unwrap().has_field(fldname)
     }
     fn close(&mut self) -> Result<()> {
-        panic!("TODO")
+        self.idx.lock().unwrap().close()?;
+        self.ts.lock().unwrap().close()
     }
 
     fn to_update_scan(&mut self) -> Result<&mut dyn UpdateScan> {
-        panic!("TODO")
+        Err(From::from(IndexSelectScanError::DowncastError))
     }
 }
