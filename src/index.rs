8aad9c99c311b61d91b42da99ae0ae6f66c94b6c
cutsea110 <cutsea110@gmail.com>
2022-03-26 15:36:42 +0900

    add tests for index update

--- src/index.rs
+++ src/index.rs
@@ -38,6 +38,7 @@ pub trait Index {
 mod tests {
     use anyhow::Result;
     use std::{
+        collections::HashMap,
         fs,
         path::Path,
         sync::{Arc, Mutex},
@@ -51,12 +52,12 @@ mod tests {
     };
 
     #[test]
-    fn unit_test() -> Result<()> {
-        if Path::new("_test/index").exists() {
-            fs::remove_dir_all("_test/index")?;
+    fn index_retrieval_test() -> Result<()> {
+        if Path::new("_test/index_retrieval").exists() {
+            fs::remove_dir_all("_test/index_retrieval")?;
         }
 
-        let db = SimpleDB::new("_test/index")?;
+        let db = SimpleDB::new("_test/index_retrieval")?;
         let tx = Arc::new(Mutex::new(db.new_tx()?));
         let mdm = db.metadata_mgr().unwrap();
 
@@ -130,6 +131,129 @@ mod tests {
         // Close the index and the data table
         idx.lock().unwrap().close()?;
         studentscan.lock().unwrap().close()?;
+        tx.lock().unwrap().commit()?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn index_update_test() -> Result<()> {
+        if Path::new("_test/index_update").exists() {
+            fs::remove_dir_all("_test/index_update")?;
+        }
+
+        let db = SimpleDB::new("_test/index_update")?;
+        let tx = Arc::new(Mutex::new(db.new_tx()?));
+        let mdm = db.metadata_mgr().unwrap();
+
+        // Create student table
+        let mut sch = Schema::new();
+        sch.add_i32_field("sid");
+        sch.add_string_field("sname", 10);
+        sch.add_i32_field("grad_year");
+        sch.add_i32_field("major_id");
+        mdm.lock()
+            .unwrap()
+            .create_table("student", Arc::new(sch), Arc::clone(&tx))?;
+
+        // Create index for major_id on student
+        mdm.lock()
+            .unwrap()
+            .create_index("idx_major_id", "student", "major_id", Arc::clone(&tx))?;
+
+        // Open an scan on the data table
+        let studentplan = TablePlan::new("student", Arc::clone(&tx), Arc::clone(&mdm))?;
+        let studentscan = studentplan.open()?;
+
+        // Create a map containing all indexes for STUDENT.
+        let mut indexes = HashMap::new();
+        let idxinfo = mdm
+            .lock()
+            .unwrap()
+            .get_index_info("student", Arc::clone(&tx))?;
+        for fldname in idxinfo.keys() {
+            let idx = idxinfo.get(fldname).unwrap().open();
+            indexes.insert(fldname, idx);
+        }
+
+        // Initialize data
+        let students = vec![
+            (1, "joe", 2021, 10),
+            (2, "amy", 2020, 20),
+            (3, "max", 2022, 10),
+            (4, "sue", 2022, 20),
+            (5, "bob", 2020, 30),
+            (6, "kim", 2020, 20),
+            (7, "art", 2021, 30),
+            (8, "pat", 2019, 20),
+            (9, "lee", 2021, 10),
+        ];
+        if let Ok(ts) = studentscan.lock().unwrap().as_table_scan() {
+            ts.before_first()?;
+            let idx = idxinfo.get("major_id").unwrap().open();
+
+            for (sid, sname, grad_year, major_id) in students {
+                ts.insert()?;
+                ts.set_i32("sid", sid)?;
+                ts.set_string("sname", sname.to_string())?;
+                ts.set_i32("grad_year", grad_year)?;
+                ts.set_i32("major_id", major_id)?;
+                idx.lock()
+                    .unwrap()
+                    .insert(Constant::I32(major_id), ts.get_rid()?)?;
+            }
+        }
+
+        // Task 1: Insert a new STUDENT record for Sam.
+        //         First, insert the record into STUDENT.
+        if let Ok(ts) = studentscan.lock().unwrap().to_update_scan() {
+            ts.insert()?;
+            ts.set_i32("sid", 11)?;
+            ts.set_string("sname", "sam".to_string())?;
+            ts.set_i32("grad_year", 2023)?;
+            ts.set_i32("major_id", 30)?;
+            //     Then insert a record into each of the indexes.
+            let datarid = ts.get_rid()?;
+            for fldname in indexes.keys() {
+                let dataval = ts.get_val(fldname)?;
+                let idx = indexes.get(fldname).unwrap();
+                idx.lock().unwrap().insert(dataval, datarid)?;
+            }
+            println!("insert sam's record");
+        }
+
+        // Task 2: Find and delete Joe's record.
+        if let Ok(ts) = studentscan.lock().unwrap().to_update_scan() {
+            ts.before_first()?;
+            while ts.next() {
+                if ts.get_string("sname")? == "joe" {
+                    // First, delete the index records for Joe.
+                    let joerid = ts.get_rid()?;
+                    for fldname in indexes.keys() {
+                        let dataval = ts.get_val(fldname)?;
+                        let idx = indexes.get(fldname).unwrap();
+                        idx.lock().unwrap().delete(dataval, joerid)?;
+                    }
+                    // Then delete Joe's record in STUDENT.
+                    ts.delete()?;
+                    println!("delete joe's record");
+                    break;
+                }
+            }
+        }
+
+        // Print the records to verify the updates.
+        if let Ok(ts) = studentscan.lock().unwrap().to_update_scan() {
+            ts.before_first()?;
+            while ts.next() {
+                println!("{} {}", ts.get_string("sname")?, ts.get_i32("sid")?)
+            }
+            ts.close()?;
+            for idx in indexes.values() {
+                idx.lock().unwrap().close()?;
+            }
+        }
+
         tx.lock().unwrap().commit()?;
 
         Ok(())
