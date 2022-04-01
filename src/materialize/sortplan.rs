e41525a4073874cc2cc0d32f283fbd1e242e1471
cutsea110 <cutsea110@gmail.com>
2022-04-01 10:10:23 +0900

    done for SortPlan

--- src/materialize/sortplan.rs
+++ src/materialize/sortplan.rs
@@ -73,6 +73,58 @@ impl SortPlan {
 
         temps
     }
+    fn do_a_merge_iteration(&self, runs: &mut Vec<TempTable>) -> Vec<TempTable> {
+        let mut result = vec![];
+        while runs.len() > 1 {
+            let p1 = runs.remove(0);
+            let p2 = runs.remove(0);
+            result.push(self.merge_two_runs(p1, p2));
+        }
+        if runs.len() == 1 {
+            result.push(runs.get(0).unwrap().clone());
+        }
+
+        result
+    }
+    fn merge_two_runs(&self, mut p1: TempTable, mut p2: TempTable) -> TempTable {
+        let src1 = p1.open().unwrap();
+        let src2 = p2.open().unwrap();
+        let mut result = TempTable::new(
+            Arc::clone(&self.next_table_num),
+            Arc::clone(&self.tx),
+            Arc::clone(&self.sch),
+        );
+        let dest = result.open().unwrap();
+
+        let mut hasmore1 = src1.lock().unwrap().next();
+        let mut hasmore2 = src2.lock().unwrap().next();
+        while hasmore1 && hasmore2 {
+            let s1 = src1.lock().unwrap().to_scan().unwrap();
+            let s2 = src2.lock().unwrap().to_scan().unwrap();
+            if self.comp.compare(Arc::clone(&s1), Arc::clone(&s2)).is_lt() {
+                hasmore1 = self.copy(s1, Arc::clone(&dest));
+            } else {
+                hasmore2 = self.copy(s2, Arc::clone(&dest));
+            }
+        }
+
+        if hasmore1 {
+            while hasmore1 {
+                let s1 = src1.lock().unwrap().to_scan().unwrap();
+                hasmore1 = self.copy(s1, Arc::clone(&dest));
+            }
+        } else {
+            while hasmore2 {
+                let s2 = src2.lock().unwrap().to_scan().unwrap();
+                hasmore2 = self.copy(s2, Arc::clone(&dest));
+            }
+        }
+        src1.lock().unwrap().close().unwrap();
+        src2.lock().unwrap().close().unwrap();
+        dest.lock().unwrap().close().unwrap();
+
+        result
+    }
     fn copy(&self, src: Arc<Mutex<dyn Scan>>, dest: Arc<Mutex<dyn UpdateScan>>) -> bool {
         dest.lock().unwrap().insert().unwrap();
         for fldname in self.sch.fields() {
@@ -83,22 +135,6 @@ impl SortPlan {
         src.lock().unwrap().next()
     }
 }
-fn do_a_merge_iteration(runs: &mut Vec<TempTable>) -> Vec<TempTable> {
-    let mut result = vec![];
-    while runs.len() > 1 {
-        let p1 = runs.remove(0);
-        let p2 = runs.remove(0);
-        result.push(merge_two_runs(p1, p2));
-    }
-    if runs.len() == 1 {
-        result.push(runs.get(0).unwrap().clone());
-    }
-
-    result
-}
-fn merge_two_runs(p1: TempTable, p2: TempTable) -> TempTable {
-    panic!("TODO")
-}
 
 impl Plan for SortPlan {
     fn open(&self) -> Result<Arc<Mutex<dyn Scan>>> {
@@ -106,7 +142,7 @@ impl Plan for SortPlan {
         let mut runs = self.split_into_runs(Arc::clone(&src));
         src.lock().unwrap().close()?;
         while runs.len() > 2 {
-            runs = do_a_merge_iteration(&mut runs);
+            runs = self.do_a_merge_iteration(&mut runs);
         }
 
         Ok(Arc::new(Mutex::new(SortScan::new(runs))))
