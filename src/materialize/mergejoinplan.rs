a26453682a3a89f25131393b5a459bda1a801828
cutsea110 <cutsea110@gmail.com>
2022-04-03 14:16:57 +0900

    implement MergeJoinPlan

--- src/materialize/mergejoinplan.rs
+++ src/materialize/mergejoinplan.rs
@@ -2,7 +2,8 @@ use anyhow::Result;
 use std::sync::{Arc, Mutex};
 
 use crate::{
-    plan::plan::Plan, query::scan::Scan, record::schema::Schema, tx::transaction::Transaction,
+    materialize::sortplan::SortPlan, plan::plan::Plan, query::scan::Scan, record::schema::Schema,
+    tx::transaction::Transaction,
 };
 
 pub struct MergeJoinPlan {
@@ -15,13 +16,30 @@ pub struct MergeJoinPlan {
 
 impl MergeJoinPlan {
     pub fn new(
+        next_table_num: Arc<Mutex<i32>>,
+
         tx: Arc<Mutex<Transaction>>,
         p1: Arc<dyn Plan>,
         p2: Arc<dyn Plan>,
         fldname1: &str,
         fldname2: &str,
     ) -> Self {
-        panic!("TODO")
+        let mut sch = Schema::new();
+        sch.add_all(p1.schema());
+        sch.add_all(p2.schema());
+
+        let sortlist1 = vec![fldname1.to_string()];
+        let plan1 = SortPlan::new(Arc::clone(&next_table_num), p1, sortlist1, Arc::clone(&tx));
+        let sortlist2 = vec![fldname2.to_string()];
+        let plan2 = SortPlan::new(Arc::clone(&next_table_num), p2, sortlist2, Arc::clone(&tx));
+
+        Self {
+            p1: Arc::new(plan1),
+            p2: Arc::new(plan2),
+            fldname1: fldname1.to_string(),
+            fldname2: fldname2.to_string(),
+            sch: Arc::new(sch),
+        }
     }
 }
 
