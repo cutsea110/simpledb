7df47da2c3932caf3a6245631577be5f84fb9908
cutsea110 <cutsea110@gmail.com>
2022-04-03 09:10:10 +0900

    GroupByPlan

--- src/materialize/groupbyplan.rs
+++ src/materialize/groupbyplan.rs
@@ -2,12 +2,15 @@ use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
 
-use super::aggregationfn::AggregationFn;
+use super::{aggregationfn::AggregationFn, groupbyscan::GroupByScan, sortplan::SortPlan};
 use crate::{
     plan::plan::Plan, query::scan::Scan, record::schema::Schema, tx::transaction::Transaction,
 };
 
 pub struct GroupByPlan {
+    // static member (shared by all Materializeplan and Temptable)
+    next_table_num: Arc<Mutex<i32>>,
+
     p: Arc<dyn Plan>,
     groupfields: Vec<String>,
     aggfns: Vec<Arc<dyn AggregationFn>>,
@@ -16,29 +19,62 @@ pub struct GroupByPlan {
 
 impl GroupByPlan {
     pub fn new(
+        next_table_num: Arc<Mutex<i32>>,
         tx: Arc<Mutex<Transaction>>,
         p: Arc<dyn Plan>,
         groupfields: Vec<String>,
         aggfns: Vec<Arc<dyn AggregationFn>>,
     ) -> Self {
-        panic!("TODO")
+        let plan = SortPlan::new(
+            Arc::clone(&next_table_num),
+            p,
+            groupfields.clone(),
+            Arc::clone(&tx),
+        );
+        let mut sch = Schema::new();
+
+        for fldname in groupfields.iter() {
+            sch.add(fldname, plan.schema());
+        }
+        for aggfn in aggfns.iter() {
+            sch.add_i32_field(&aggfn.field_name());
+        }
+        Self {
+            next_table_num,
+            p: Arc::new(plan),
+            groupfields,
+            aggfns,
+            sch: Arc::new(sch),
+        }
     }
 }
 
 impl Plan for GroupByPlan {
     fn open(&self) -> Result<Arc<Mutex<dyn Scan>>> {
-        panic!("TODO")
+        let s = self.p.open()?;
+        let scan = GroupByScan::new(s, self.groupfields.clone(), self.aggfns.clone());
+
+        Ok(Arc::new(Mutex::new(scan)))
     }
     fn blocks_accessed(&self) -> i32 {
-        panic!("TODO")
+        self.p.blocks_accessed()
     }
     fn records_output(&self) -> i32 {
-        panic!("TODO")
+        let mut numgroups = 1;
+        for fldname in self.groupfields.iter() {
+            numgroups *= self.p.distinct_values(fldname);
+        }
+
+        numgroups
     }
     fn distinct_values(&self, fldname: &str) -> i32 {
-        panic!("TODO")
+        if self.p.schema().has_field(fldname) {
+            self.p.distinct_values(fldname)
+        } else {
+            self.records_output()
+        }
     }
     fn schema(&self) -> Arc<Schema> {
-        panic!("TODO")
+        Arc::clone(&self.sch)
     }
 }
--- src/materialize/groupbyscan.rs
+++ src/materialize/groupbyscan.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
 
@@ -18,7 +18,7 @@ pub struct GroupByScan {
 
 impl GroupByScan {
     pub fn new(
-        s: Arc<dyn Scan>,
+        s: Arc<Mutex<dyn Scan>>,
         groupfields: Vec<String>,
         aggfns: Vec<Arc<dyn AggregationFn>>,
     ) -> Self {
