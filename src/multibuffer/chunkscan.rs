66793443c4546bca035108221cca53f12cd8261a
cutsea110 <cutsea110@gmail.com>
2022-04-06 18:22:10 +0900

    implement ChunkScan

--- src/multibuffer/chunkscan.rs
+++ src/multibuffer/chunkscan.rs
@@ -1,12 +1,30 @@
+use anyhow::Result;
+use core::fmt;
 use std::sync::{Arc, Mutex};
 
-use anyhow::Result;
-
 use crate::{
+    file::block_id::BlockId,
     query::{constant::Constant, scan::Scan},
+    record::schema::FieldType,
     record::{layout::Layout, recordpage::RecordPage},
     tx::transaction::Transaction,
 };
+
+#[derive(Debug)]
+pub enum ChunkScanError {
+    DowncastError,
+}
+
+impl std::error::Error for ChunkScanError {}
+impl fmt::Display for ChunkScanError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ChunkScanError::DowncastError => {
+                write!(f, "downcast error")
+            }
+        }
+    }
+}
 
 pub struct ChunkScan {
     buffs: Vec<RecordPage>,
@@ -16,7 +34,7 @@ pub struct ChunkScan {
     startbnum: i32,
     endbnum: i32,
     currentbnum: i32,
-    rp: RecordPage,
+    rp: Option<RecordPage>,
     currentslot: i32,
 }
 
@@ -28,39 +46,89 @@ impl ChunkScan {
         startbnum: i32,
         endbnum: i32,
     ) -> Self {
-        panic!("TODO")
+        let mut buffs: Vec<RecordPage> = vec![];
+        for i in startbnum..=endbnum {
+            let blk = BlockId::new(&filename, i);
+            let rp = RecordPage::new(Arc::clone(&tx), blk, Arc::clone(&layout)).unwrap();
+            buffs.push(rp);
+        }
+
+        let mut scan = Self {
+            buffs,
+            tx,
+            filename,
+            layout,
+            startbnum,
+            endbnum,
+            currentbnum: 0,
+            rp: None,
+            currentslot: 0,
+        };
+        scan.move_to_block(startbnum);
+
+        scan
+    }
+    fn move_to_block(&mut self, blknum: i32) {
+        self.currentbnum = blknum;
+        self.rp = self
+            .buffs
+            .get((self.currentbnum - self.startbnum) as usize)
+            .map(|rp| rp.clone());
+        self.currentslot = -1;
     }
 }
 
 impl Scan for ChunkScan {
     fn before_first(&mut self) -> Result<()> {
-        panic!("TODO")
+        self.move_to_block(self.startbnum);
+
+        Ok(())
     }
     fn next(&mut self) -> bool {
-        panic!("TODO")
+        let rp = self.rp.as_mut().unwrap();
+        self.currentslot = rp.next_after(self.currentslot).unwrap();
+        while self.currentslot < 0 {
+            if self.currentbnum == self.endbnum {
+                return false;
+            }
+            self.move_to_block(self.rp.as_ref().unwrap().block().number() + 1);
+            let rp = self.rp.as_mut().unwrap();
+            self.currentslot = rp.next_after(self.currentslot).unwrap();
+        }
+
+        true
     }
     fn get_i32(&mut self, fldname: &str) -> Result<i32> {
-        panic!("TODO")
+        self.rp.as_mut().unwrap().get_i32(self.currentslot, fldname)
     }
     fn get_string(&mut self, fldname: &str) -> Result<String> {
-        panic!("TODO")
+        let rp = self.rp.as_mut().unwrap();
+        rp.get_string(self.currentslot, fldname)
     }
     fn get_val(&mut self, fldname: &str) -> Result<Constant> {
-        panic!("TODO")
+        match self.layout.schema().field_type(fldname) {
+            FieldType::INTEGER => Ok(Constant::new_i32(self.get_i32(fldname)?)),
+            FieldType::VARCHAR => Ok(Constant::new_string(self.get_string(fldname)?)),
+        }
     }
     fn has_field(&self, fldname: &str) -> bool {
-        panic!("TODO")
+        self.layout.schema().has_field(fldname)
     }
     fn close(&mut self) -> Result<()> {
-        panic!("TODO")
+        for i in 0..self.buffs.len() {
+            let blk = BlockId::new(&self.filename, self.startbnum + i as i32);
+            self.tx.lock().unwrap().unpin(&blk)?;
+        }
+
+        Ok(())
     }
     fn to_update_scan(&mut self) -> Result<&mut dyn crate::query::updatescan::UpdateScan> {
-        panic!("TODO")
+        Err(From::from(ChunkScanError::DowncastError))
     }
     fn as_table_scan(&mut self) -> Result<&mut crate::record::tablescan::TableScan> {
-        panic!("TODO")
+        Err(From::from(ChunkScanError::DowncastError))
     }
     fn as_sort_scan(&mut self) -> Result<&mut crate::materialize::sortscan::SortScan> {
-        panic!("TODO")
+        Err(From::from(ChunkScanError::DowncastError))
     }
 }
