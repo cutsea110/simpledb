45c5e9ec3780eed36b18366367f7f4aa07d95384
cutsea110 <cutsea110@gmail.com>
2022-04-13 11:14:41 +0900

    add tests for SelectPlan

--- src/plan/selectplan.rs
+++ src/plan/selectplan.rs
@@ -53,3 +53,66 @@ impl SelectPlan {
         Self { p, pred }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Result;
+    use std::{fs, path::Path};
+
+    use super::*;
+    use crate::{
+        metadata::manager::MetadataMgr,
+        plan::{plan::Plan, tableplan::TablePlan},
+        query::{constant::Constant, expression::Expression, term::Term, tests},
+        server::simpledb::SimpleDB,
+    };
+
+    #[test]
+    fn unit_test() -> Result<()> {
+        if Path::new("_test/selectplan").exists() {
+            fs::remove_dir_all("_test/selectplan")?;
+        }
+
+        let simpledb = SimpleDB::new_with("_test/selectplan", 400, 8);
+
+        let tx = Arc::new(Mutex::new(simpledb.new_tx()?));
+        assert_eq!(tx.lock().unwrap().available_buffs(), 8);
+
+        let mut mdm = MetadataMgr::new(true, Arc::clone(&tx))?;
+        assert_eq!(tx.lock().unwrap().available_buffs(), 8);
+
+        tests::init_sampledb(&mut mdm, Arc::clone(&tx))?;
+        assert_eq!(tx.lock().unwrap().available_buffs(), 8);
+
+        let mdm = Arc::new(Mutex::new(mdm));
+        assert_eq!(tx.lock().unwrap().available_buffs(), 8);
+
+        let srcplan = Arc::new(TablePlan::new(
+            "STUDENT",
+            Arc::clone(&tx),
+            Arc::clone(&mdm),
+        )?);
+        assert_eq!(tx.lock().unwrap().available_buffs(), 8);
+
+        let pred = Predicate::new(Term::new(
+            Expression::Fldname("GradYear".to_string()),
+            Expression::Val(Constant::I32(2020)),
+        ));
+        let plan = SelectPlan::new(srcplan, pred);
+
+        println!("PLAN: {}", plan.dump());
+        let scan = plan.open()?;
+        scan.lock().unwrap().before_first()?;
+        let mut iter = scan.lock().unwrap();
+        while iter.next() {
+            let name = iter.get_string("SName")?;
+            let year = iter.get_i32("GradYear")?;
+            let major_id = iter.get_i32("MajorId")?;
+            println!("{:<10}{:>8}{:>8}", name, major_id, year);
+        }
+        tx.lock().unwrap().commit()?;
+        assert_eq!(tx.lock().unwrap().available_buffs(), 8);
+
+        Ok(())
+    }
+}
