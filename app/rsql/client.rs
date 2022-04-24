e05e0a2d63250f63c8995751180361c39fe90511
cutsea110 <cutsea110@gmail.com>
2022-04-25 07:21:07 +0900

    Revert "ping"
    This reverts commit 2a74aba8d25dc66465dbf0106801e512705ae349.

--- app/rsql/client.rs
+++ app/rsql/client.rs
@@ -1,57 +1,5 @@
-use capnp_rpc::{rpc_twoparty_capnp, twoparty, RpcSystem};
-use futures::{AsyncReadExt, FutureExt};
-use std::{error::Error, net::ToSocketAddrs};
-
-use crate::test_capnp::ping;
+use std::error::Error;
 
 pub async fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = ::std::env::args().collect();
-    if args.len() != 4 {
-        println!("usage: {} client HOST:PORT MESSAGE", args[0]);
-        return Ok(());
-    }
-
-    let addr = args[2]
-        .to_socket_addrs()
-        .unwrap()
-        .next()
-        .expect("could not parse address");
-    let msg = args[3].to_string();
-
-    tokio::task::LocalSet::new()
-        .run_until(async move {
-            let stream = tokio::net::TcpStream::connect(&addr).await?;
-            stream.set_nodelay(true)?;
-            let (reader, writer) =
-                tokio_util::compat::TokioAsyncReadCompatExt::compat(stream).split();
-            let rpc_network = Box::new(twoparty::VatNetwork::new(
-                reader,
-                writer,
-                rpc_twoparty_capnp::Side::Client,
-                Default::default(),
-            ));
-            let mut rpc_system = RpcSystem::new(rpc_network, None);
-            let ping_client: ping::Client = rpc_system.bootstrap(rpc_twoparty_capnp::Side::Server);
-
-            tokio::task::spawn_local(Box::pin(rpc_system.map(|_| ())));
-
-            let mut request = ping_client.ping_request();
-            request.get().init_request().set_name(&msg);
-
-            let reply = request.send().promise.await.unwrap();
-
-            println!(
-                "received: {}",
-                reply
-                    .get()
-                    .unwrap()
-                    .get_reply()
-                    .unwrap()
-                    .get_message()
-                    .unwrap()
-            );
-
-            Ok(())
-        })
-        .await
+    panic!("TODO")
 }
--- app/rsql/server.rs
+++ app/rsql/server.rs
@@ -1,7 +1,5 @@
 use capnp::capability::Promise;
-use capnp_rpc::{rpc_twoparty_capnp, twoparty, RpcSystem};
-use futures::{AsyncReadExt, FutureExt};
-use std::{error::Error, net::ToSocketAddrs};
+use std::error::Error;
 
 use crate::test_capnp::ping;
 
@@ -10,53 +8,13 @@ struct PingImpl;
 impl ping::Server for PingImpl {
     fn ping(
         &mut self,
-        params: ping::PingParams,
-        mut results: ping::PingResults,
+        _params: ping::PingParams,
+        mut _results: ping::PingResults,
     ) -> Promise<(), capnp::Error> {
-        let request = params.get().unwrap().get_request().unwrap();
-        let name = request.get_name().unwrap();
-        let message = format!("Hello, {}", name);
-
-        println!("received: {}", message);
-        results.get().init_reply().set_message(&message);
-
-        Promise::ok(())
+        panic!("TODO")
     }
 }
 
 pub async fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = ::std::env::args().collect();
-    if args.len() != 3 {
-        println!("usage: {} server ADDRESS[:PORT]", args[0]);
-    }
-
-    let addr = args[2]
-        .to_socket_addrs()
-        .unwrap()
-        .next()
-        .expect("could not parse address");
-
-    tokio::task::LocalSet::new()
-        .run_until(async move {
-            let listener = tokio::net::TcpListener::bind(&addr).await?;
-            let ping_client: ping::Client = capnp_rpc::new_client(PingImpl);
-
-            loop {
-                let (stream, _) = listener.accept().await?;
-                stream.set_nodelay(true)?;
-                let (reader, writer) =
-                    tokio_util::compat::TokioAsyncReadCompatExt::compat(stream).split();
-                let network = twoparty::VatNetwork::new(
-                    reader,
-                    writer,
-                    rpc_twoparty_capnp::Side::Server,
-                    Default::default(),
-                );
-
-                let rpc_system =
-                    RpcSystem::new(Box::new(network), Some(ping_client.clone().client));
-                tokio::task::spawn_local(Box::pin(rpc_system.map(|_| ())));
-            }
-        })
-        .await
+    panic!("TODO")
 }
