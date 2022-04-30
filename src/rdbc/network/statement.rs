952d0a0d4a89ea9e7c5275160d823220328b2dfb
cutsea110 <cutsea110@gmail.com>
2022-04-30 17:31:21 +0900

    network statement

--- src/rdbc/network/statement.rs
+++ src/rdbc/network/statement.rs
@@ -1,10 +1,22 @@
 use anyhow::Result;
 
 use super::{planrepr::NetworkPlanRepr, resultset::NetworkResultSet};
-use crate::rdbc::statementadapter::StatementAdapter;
+use crate::{
+    rdbc::statementadapter::StatementAdapter,
+    remote_capnp::{
+        remote_result_set,
+        remote_statement::{self, *},
+    },
+};
 
 pub struct NetworkStatement {
-    // TODO
+    client: remote_statement::Client,
+}
+
+impl NetworkStatement {
+    pub fn new(client: remote_statement::Client) -> Self {
+        Self { client }
+    }
 }
 
 impl<'a> StatementAdapter<'a> for NetworkStatement {
@@ -12,7 +24,13 @@ impl<'a> StatementAdapter<'a> for NetworkStatement {
     type PlanRepr = NetworkPlanRepr;
 
     fn execute_query(&'a mut self) -> Result<Self::Set> {
-        panic!("TODO")
+        let rt = tokio::runtime::Runtime::new().unwrap(); // TODO
+        let resultset = rt.block_on(async {
+            let request = self.client.execute_query_request();
+            request.send().pipeline.get_result()
+        });
+
+        Ok(NetworkResultSet::new(resultset))
     }
     fn execute_update(&mut self) -> Result<i32> {
         panic!("TODO")
