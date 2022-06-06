05f7be141f7345d511c1b4f7ef38d74959aa2e55
cutsea110 <cutsea110@gmail.com>
2022-06-07 07:06:29 +0900

    mod metacmd

--- app/embedded/main.rs
+++ app/embedded/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use env_logger::{self, Env};
-use itertools::Itertools;
+use metacmd::exec_meta_cmd;
 use std::{
     io::{stdout, Write},
     path::Path,
@@ -15,13 +15,11 @@ use simpledb::rdbc::{
 };
 
 use execquery::exec_query;
-use explainplan::print_explain_plan;
-use tableschema::print_table_schema;
 use updatecmd::exec_update_cmd;
-use viewdef::print_view_definition;
 
 pub mod execquery;
 pub mod explainplan;
+pub mod metacmd;
 pub mod tableschema;
 pub mod updatecmd;
 pub mod viewdef;
@@ -75,77 +73,6 @@ fn confirm_new_db(dbname: &str) {
     if ans != "y" && ans != "yes" {
         println!("terminates the process.");
         process::exit(0);
-    }
-}
-
-fn print_help_meta_cmd() {
-    println!(":h, :help                       Show this help");
-    println!(":q, :quit, :exit                Quit the program");
-    println!(":t, :table   <table_name>       Show table schema");
-    println!(":v, :view    <view_name>        Show view definition");
-    println!(":e, :explain <sql>              Explain plan");
-}
-
-fn exec_meta_cmd(conn: &mut EmbeddedConnection, qry: &str) {
-    let tokens: Vec<&str> = qry.trim().split_whitespace().collect_vec();
-    let cmd = tokens[0].to_ascii_lowercase();
-    let args = &tokens[1..];
-    match cmd.as_str() {
-        ":h" | ":help" => {
-            print_help_meta_cmd();
-        }
-        ":q" | ":quit" | ":exit" => {
-            conn.close().expect("close");
-            println!("disconnected.");
-            process::exit(0);
-        }
-        ":t" | ":table" => {
-            if args.is_empty() {
-                println!("table name is required.");
-                return;
-            }
-            let tblname = args[0];
-            if let Ok(sch) = conn.get_table_schema(tblname) {
-                let idx_info = conn.get_index_info(tblname).unwrap_or_default();
-                print_table_schema(tblname, sch, idx_info);
-            }
-            return;
-        }
-        ":v" | ":view" => {
-            if args.is_empty() {
-                println!("view name is required.");
-                return;
-            }
-            let viewname = args[0];
-            if let Ok((viewname, viewdef)) = conn.get_view_definition(viewname) {
-                print_view_definition(&viewname, &viewdef);
-                println!();
-            }
-            return;
-        }
-        ":e" | ":explain" => {
-            if args.is_empty() {
-                println!("SQL is required.");
-                return;
-            }
-            let sql = qry[tokens[0].len()..].trim();
-            let mut stmt = conn.create_statement(sql).expect("create statement");
-            let words: Vec<&str> = sql.split_whitespace().collect();
-            if !words.is_empty() {
-                let cmd = words[0].trim().to_ascii_lowercase();
-                if &cmd == "select" {
-                    if let Ok(plan_repr) = stmt.explain_plan() {
-                        print_explain_plan(plan_repr);
-                        println!();
-                        return;
-                    }
-                }
-            }
-            println!("expect query(not command).");
-        }
-        cmd => {
-            println!("Unknown command: {}", cmd)
-        }
     }
 }
 
