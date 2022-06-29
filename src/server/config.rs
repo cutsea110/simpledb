636e049d1379feff73424dc1ee34ae67cf03c894
cutsea110 <cutsea110@gmail.com>
2022-06-29 16:27:53 +0900

    make SimpleDBConfig

--- src/server/config.rs
+++ src/server/config.rs
@@ -1,15 +1,18 @@
-#[derive(Debug, Clone)]
-pub struct Config {
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct SimpleDBConfig {
     // configurable parameters
     //
+    // File Manager
+    pub block_size: i32,
     // Buffer Manager
-    buffer_manager: BufferMgr,
+    pub num_of_buffers: usize,
+    pub buffer_manager: BufferMgr,
     // query planner
-    query_planner: QueryPlanner,
+    pub query_planner: QueryPlanner,
 }
 
-#[derive(Debug, Clone)]
-enum BufferMgr {
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum BufferMgr {
     Naive,
     NaiveBis,
     FIFO,
@@ -17,8 +20,8 @@ enum BufferMgr {
     Clock,
 }
 
-#[derive(Debug, Clone)]
-enum QueryPlanner {
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum QueryPlanner {
     Basic,
     Heuristic,
 }
