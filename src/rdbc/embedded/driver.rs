98853837d961f5880fdc38c6c1635737f5d828e5
cutsea110 <cutsea110@gmail.com>
2022-07-01 18:16:11 +0900

    embedded version is build up from Config

--- src/rdbc/embedded/driver.rs
+++ src/rdbc/embedded/driver.rs
@@ -3,14 +3,26 @@ use anyhow::Result;
 use super::connection::EmbeddedConnection;
 use crate::{
     rdbc::driveradapter::{DriverAdapter, DriverError},
-    server::simpledb::SimpleDB,
+    server::{
+        config::{self, SimpleDBConfig},
+        simpledb::{self, SimpleDB},
+    },
 };
 
-pub struct EmbeddedDriver;
+pub struct EmbeddedDriver {
+    cfg: SimpleDBConfig,
+}
 
 impl EmbeddedDriver {
     pub fn new() -> Self {
-        Self
+        Self {
+            cfg: SimpleDBConfig {
+                block_size: simpledb::BLOCK_SIZE,
+                num_of_buffers: simpledb::BUFFER_SIZE,
+                buffer_manager: config::BufferMgr::LRU,
+                query_planner: config::QueryPlanner::Heuristic,
+            },
+        }
     }
 }
 
@@ -18,7 +30,7 @@ impl DriverAdapter<'_> for EmbeddedDriver {
     type Con = EmbeddedConnection;
 
     fn connect(&self, dbname: &str) -> Result<Self::Con> {
-        if let Ok(db) = SimpleDB::new(dbname) {
+        if let Ok(db) = SimpleDB::build_from(self.cfg.clone())(dbname) {
             return Ok(EmbeddedConnection::new(db));
         }
 
