79a2bb3a44fa257430f002cd9b091157446ddd33
cutsea110 <cutsea110@gmail.com>
2022-07-07 07:42:07 +0900

    refactor

--- benchmarks.sh
+++ benchmarks.sh
@@ -11,24 +11,17 @@ if [ ! -e ./esql ]; then
     ln -s ../target/debug/esql
 fi
 
-# default tiny
-if [ -d ./tiny ]; then
-    rm -rf ./tiny
-fi
-./gen-sql-for-db.sh tiny > ./init-tiny.sql
-./e2e.sh tiny
-
-# create small size db init sql file
-## ./gen-sql-for-db.sh small > ./init-small.sql
-## ./e2e.sh small
-
-# create medium size db init sql file
-## ./gen-sql-for-db.sh medium > ./init-medium.sql
-## ./e2e.sh medium
-
-# create large size db init sql file
-## ./gen-sql-for-db.sh large > ./init-large.sql
-## ./e2e.sh large
+#
+# Any combination of tiny small medium large can be specified.
+#
+for scale in tiny
+do
+    if [ -d ./${scale} ]; then
+	rm -rf ./${scale}
+    fi
+    ./gen-sql-for-db.sh ${scale} > ./init-${scale}.sql
+    ./e2e.sh ${scale}
+done
 
 rm esql
 
